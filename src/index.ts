7213764c986134fd6be2c883221c115bad8c235b
Alex Uribe <repzyyog@gmail.com>
2025-02-14 21:26:39 +0100

    Resolved sort number

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-const listOfYears: number[] = [];
+const years: number[] = [];
 for (let year = 2000; year <= 2025; year++) {
-  listOfYears.push(year);
+  years.push(year);
 }
-console.log(`Lista de años desde el 1999: ${listOfYears.join(", ")}`);
+console.log(`Lista de años desde el 1999: ${years.join(", ")}`);
 console.log("\n");
 
 const words = [
@@ -49,25 +49,26 @@ console.log(words.reverse().join(" "));
 console.log("\n");
 
 let examResults: number[] = [];
-examResults = [5, 3.2, 7, 8.6, 4];
+examResults = [5, 3.2, 7, 8.6, 4, 10, 9.9, 0];
 
-let sortedExamResults = examResults.sort();
+let sortedExamResults = examResults.sort(function (a, b) {
+  return a - b;
+});
 
-let passExamResult;
-for (const individualExamResult of sortedExamResults) {
-  if (individualExamResult >= 5) {
-    passExamResult = `${passExamResult}, ${individualExamResult}`;
-  }
-}
+let passExamResult = sortedExamResults.filter(
+  (sortedExamResults) => sortedExamResults >= 5
+);
+
 console.log(`Los examenes aprobados han sacado estas notas: ${passExamResult}`);
 
 let sumOfResults = 0;
 for (const individualExamResult of examResults) {
   sumOfResults = sumOfResults + individualExamResult;
 }
-console.log(
-  `La nota media del examen ha sido ${sumOfResults / examResults.length}!`
-);
+
+const averageExams = sumOfResults / examResults.length;
+
+console.log(`La nota media del examen ha sido ${averageExams.toFixed(2)}!`);
 
 console.log(
   `La nota mas baja del examen ha sido un ${sortedExamResults.at(
